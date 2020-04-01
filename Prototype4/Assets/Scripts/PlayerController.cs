6fdee56d5178d01769ad3cc697f143ac5dec2be5
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-31 23:35:33 -0500

    Added script to allow the player to rotate the main camera along a focal point, added a player controller that detects the player vertical input movement

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    private  Rigidbody playerRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+
+        playerRigidBody.AddForce(Vector3.forward * verticalInput * movementSpeed);
+    }
+}
