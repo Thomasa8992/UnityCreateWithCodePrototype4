523543ece2069ef1d7116bc23c86d787f3313205
Adrian Thomas <thomasa8992@hotmail.com>
2020-04-02 01:36:29 -0500

    Added a spawn manager game object and script and added logic to spawn an enemy at a random location on the map. Also added logic to make the enemy follow the player around the map

---
+++ Prototype4/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody enemyRigidBody;
+    private GameObject player;
+    private float movementSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRigidBody.AddForce(lookDirection * movementSpeed);
+    }
+}
---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateEnemy", 5, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private Vector3 GenerateSpawnPosition() {
+        var spawnPositionX = Random.Range(-spawnRange, spawnRange);
+        var spawnPositionZ = Random.Range(-spawnRange, spawnRange);
+
+        var randomPosition = new Vector3(spawnPositionX, 0, spawnPositionZ);
+
+        return randomPosition;
+    }
+
+    private void CreateEnemy() {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+}
