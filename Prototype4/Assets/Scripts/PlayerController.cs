1feb968997e9e182135b648713a6d4e3eb1d7934
Adrian Thomas <thomasa8992@hotmail.com>
2020-04-02 22:05:29 -0500

    Added a new object that represents a powerup, added object to prefab, added logic that manipulates the player and enemy force to make the enemy move futher back when they collide

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 5.0f;
     private  Rigidbody playerRigidBody;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    public float powerupStrength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,30 @@ public class PlayerController : MonoBehaviour
         var verticalInput = Input.GetAxis("Vertical");
 
         playerRigidBody.AddForce(focalPoint.transform.forward * verticalInput * movementSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Powerup")) {
+            hasPowerUp = true;
+
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine() {
+        yield return new WaitForSeconds(5);
+
+        hasPowerUp = false;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp) {
+            var enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            var enemyCurrentPosition = collision.gameObject.transform.position - transform.position;
+            var calculatedPowerupKnockbackForce = enemyCurrentPosition * powerupStrength;
+
+            enemyRigidBody.AddForce(calculatedPowerupKnockbackForce, ForceMode.Impulse);
+        }
     }
 }
